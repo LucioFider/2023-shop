38efd19ba5eda4728adf8c5b557cd713139ba4d7
LucioFider <fidelislucio404@gmail.com>
2023-02-11 23:21:39 +1000

    4th-commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+  ShoppingCartIcon,
+  HeartIcon,
+} from "@heroicons/react/24/outline";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="flag" />
+            <ChevronDownIcon className="nav-icon" />
+          </div>
+          <div className="item">
+            <span>PGK</span>
+            <ChevronDownIcon className="nav-icon" />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">Sello Theme</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="item">
+            <Link to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <MagnifyingGlassIcon className="nav-icon" />
+            <UserIcon className="nav-icon" />
+            <HeartIcon className="nav-icon" />
+            <div className="cartIcon">
+              <ShoppingCartIcon className="nav-icon" />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
