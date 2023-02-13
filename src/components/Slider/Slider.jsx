a0e62e9ac971afa7be08c8ee01402c3906fdcf59
LucioFider <fidelislucio404@gmail.com>
2023-02-13 10:11:56 +1000

    7th-commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./slider.scss";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = ["/img/slide1.png", "/img/slide2.png", "/img/slide3.png"];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
 
   return (
     <div className="slider">
-      <div className="container">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="slide1" />
         <img src={data[1]} alt="slide2" />
         <img src={data[2]} alt="slide3" />
       </div>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={prevSlide}>
           <ArrowLeftIcon />
         </div>
-        <div className="icon">
+        <div className="icon" onClick={nextSlide}>
           <ArrowRightIcon />
         </div>
       </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Slider } from "../../components";
+import "./home.scss";
 
 const Home = () => {
   return (
